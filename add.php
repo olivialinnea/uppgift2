f7896556d096ed3f2c0b52e811c149de91b4b628
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2021-10-25 16:52:50 +0200

    Går att lägga till hundar

--- add.php
+++ add.php
@@ -10,3 +10,40 @@ if (!isset($_SESSION["loggedIn"])) {
     header("Location: /check-login.php");
     exit();
 }
+
+if (isset($_POST["dogName"], $_POST["breed"], $_POST["age"], $_POST["notes"])) {
+    $name = $_POST["dogName"];
+    $breed = $_POST["breed"];
+    $age = $_POST["age"];
+    $notes = $_POST["notes"];
+
+    if ($name == "" || $breed == "" || $age == "" || $notes == "") {
+        header("Location: /add.php?error=1");
+        exit();
+    }
+    addNewDog($_POST);
+}
+?>
+
+<div id="add">
+    <h2 class="title">Add your doggo here!</h2>
+    <form action="/add.php" method="POST">
+        <input type="text" name="dogName" placeholder="Name">
+        <input type="text" name="breed" placeholder="Breed">
+        <input type="number" min="0" name="age" placeholder="Age">
+        <input type="text" name="notes" placeholder="Notes about them">
+        <button type="submit">Add</button>
+    </form>
+    <?php
+    if (isset($_POST["dogName"], $_POST["breed"], $_POST["age"], $_POST["notes"])) { ?>
+        <p>You added <?php echo $_POST["dogName"] ?>, who is a <?php echo $_POST["age"] ?> year old <?php echo $_POST["breed"] ?>!</p>
+    <?php } ?>
+
+    <?php
+    if (isset($_GET["error"])) {
+        $error = $_GET["error"]; ?>
+        <?php if ($error == 1) { ?>
+            <p class="error">Please leave no empty fields!</p>
+        <?php } ?>
+    <?php } ?>
+</div>
--- includes/functions.php
+++ includes/functions.php
@@ -31,4 +31,138 @@ function getDogsFromDB()
 
     return $everyDog;
 }
+
+//Lägger till en ny hund i databasen
+function addNewDog($postInfo)
+{
+    //nycklar för den nya hunden, från $_POST
+    $newDog = [
+        "name" => $postInfo["dogName"],
+        "breed" => $postInfo["breed"],
+        "age" => $postInfo["age"],
+        "notes" => $postInfo["notes"]
+    ];
+    //denna foreachen räknar ut den nya 
+    //hundens id utifrån vilka som redan finns
+    $highestID = 0;
+
+    $allDogs = getDogsFromDB();
+    foreach ($allDogs as $dog) {
+        if ($dog["id"] > $highestID) {
+            $highestID = $dog["id"];
+        }
+    }
+    //owner till den nya hunden, vilket är den som är inloggad
+    $newDog["owner"] = $_SESSION["id"];
+    //ID:et av den nya hunden
+    $newDog["id"] = $highestID + 1;
+    //lägg till hund i db.json
+    $data = json_decode(file_get_contents("db.json"), true);
+    array_push($data["dogs"], $newDog);
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents("db.json", $json);
+}
+
+function deleteTheDog($dogID)
+{
+    $allOurDogs = getDogsFromDB();
+
+    $found = false;
+    foreach ($allOurDogs as $key => $dog) {
+        if ($dogID == $dog["id"]) {
+            $found = true;
+            $index = $key;
+            break;
+        }
+    }
+
+    if ($found) {
+        $data = json_decode(file_get_contents("db.json"), true);
+        unset($data["dogs"][$index]);
+        $json = json_encode($data, JSON_PRETTY_PRINT);
+        file_put_contents("db.json", $json);
+    }
+
+    header("Location: /profile.php");
+    exit();
+}
+
+function showDog($dogInfo)
+{
+    $everyUser = getUsersFromDB();
+
+    foreach ($everyUser as $user) {
+        if ($user["id"] == $dogInfo["owner"]) {
+            $nameOfOwner = $user["username"];
+            break;
+        } else {
+            $nameOfOwner = "<p>No Owner.</p>";
+        }
+    }
+    //om vi är inne på profile ska deleteknappen finnas
+    if (checkURL("profile") == true) {
+        $dogDiv = "
+        <div class='oneDog'>
+            <div class='name'>
+                <p>NAME</p>
+                <p><a href='show.php?id={$dogInfo[' id']}'>{$dogInfo['name']}</a></p>
+            </div>
+            <div class='breed'>
+                <p>BREED</p>
+                <p><a href='list.php?breed={$dogInfo[' breed']}'>{$dogInfo['breed']}</a></p>
+            </div>
+            <div class='age'>
+                <p>AGE</p>
+                <p>{$dogInfo['age']}</p>
+            </div>
+            <div class='notes'>
+                <p>NOTES</p>
+                <p>{$dogInfo['notes']}</p>
+            </div>
+            <div class='owner'>
+                <p>OWNER</p>
+                <p>{$nameOfOwner}</p>
+            </div>
+            <div class='delete'>
+                <p>ACTION</p>
+                <p><a href='delete.php?id={$dogInfo[' id']}'>DELETE</a></p>
+            </div>
+        </div>";
+    } else {
+        $dogDiv = "
+        <div class='oneDog' style='grid-template-columns: repeat(5, 1fr);'>
+            <div class='name'>
+                <p>NAME</p>
+                <p><a href='show.php?id={$dogInfo[' id']}'>{$dogInfo['name']}</a></p>
+            </div>
+            <div class='breed'>
+                <p>BREED</p>
+                <p><a href='list.php?breed={$dogInfo[' breed']}'>{$dogInfo['breed']}</a></p>
+            </div>
+            <div class='age'>
+                <p>AGE</p>
+                <p>{$dogInfo['age']}</p>
+            </div>
+            <div class='notes'>
+                <p>NOTES</p>
+                <p>{$dogInfo['notes']}</p>
+            </div>
+            <div class='owner'>
+                <p>OWNER</p>
+                <p>{$nameOfOwner}</p>
+            </div>
+        </div>";
+    }
+    return $dogDiv;
+}
+
+
+function checkURL($string)
+{
+    if (strpos($_SERVER['REQUEST_URI'], "$string") !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
 ?>
